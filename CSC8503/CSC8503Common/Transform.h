4968cb3159ccb9dda4163d8807a9844a5b030e6a
Crypt <t.meng3@newcastle.ac.uk>
2021-02-13 20:31:59 +0800

    Add support functions to get up, down, left, right, forward, backward direction of transform

--- CSC8503/CSC8503Common/Transform.h
+++ CSC8503/CSC8503Common/Transform.h
@@ -21,6 +21,36 @@ namespace NCL {
 			Transform& SetPosition(const Vector3& worldPos);
 			Transform& SetScale(const Vector3& worldScale);
 			Transform& SetOrientation(const Quaternion& newOr);
+			
+			Vector3 Up() const
+			{
+				return Vector3(matrix.GetColumn(1));
+			}
+
+			Vector3 Down() const
+			{
+				return Vector3(-matrix.GetColumn(1));
+			}
+
+			Vector3 Left() const
+			{
+				return Vector3(-matrix.GetColumn(0));
+			}
+
+			Vector3 Right() const
+			{
+				return Vector3(matrix.GetColumn(0));
+			}
+
+			Vector3 Forward() const
+			{
+				return Vector3(-matrix.GetColumn(2));
+			}
+
+			Vector3 Backward() const
+			{
+				return Vector3(matrix.GetColumn(2));
+			}
 
 			Vector3 GetPosition() const {
 				return position;
