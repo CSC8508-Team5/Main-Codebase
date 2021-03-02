750f48837a359852fe8a5b1587c8b9012c45d224
Crypt <t.meng3@newcastle.ac.uk>
2021-03-03 01:08:09 +0800

    add sound source class

---
+++ CSC8503/CSC8503Common/SoundSource.h
@@ -0,0 +1,133 @@
+#pragma once
+
+#include "AudioSystem.h"
+
+namespace NCL {
+	namespace CSC8503 {
+		class SoundSource
+		{
+		public:
+			SoundSource(string filename, bool loop = false, bool paused = false)
+			{
+				InitSound(filename, loop, paused);
+			}
+			SoundSource()
+			{
+				sound = 0;
+				fxControl = 0;
+			}
+			~SoundSource()
+			{
+				if (!sound)
+					return;
+				sound->drop();
+				delete sound;
+			}
+
+			void InitSound(string filename, bool loop, bool pausedOnInit = false)
+			{
+				if (sound)
+				{
+					sound->drop();
+				}
+				sound = AudioSystem::AddSound(filename,Vector3::Zero(), loop, pausedOnInit);
+
+				this->soundFile = filename;
+				this->isLoop = loop;
+
+				UpdateParams();
+
+				fxControl = sound->getSoundEffectControl();
+				if (!fxControl)
+					std::cout << "This device or sound does not support sound effects.\n";
+			}
+
+			ISoundEffectControl* GetSoundFxControl() { return fxControl; }
+
+			float GetMinDistance() { return minDistance; }
+			float GetMaxDistance() { return maxDistance; }
+			float GetVolume() { return volume; }
+			bool GetIsLoop() { return isLoop; }
+
+			void SetMinDistance(float d) { if (d != minDistance)	sound->setMinDistance(d); }
+			void SetMaxDistance(float d) { if (d != maxDistance)	sound->setMaxDistance(d); }
+			void SetVolume(float vol) { if (vol != volume) sound->setVolume(vol); }
+			void SetIsLoop(bool loop) { if (loop != isLoop) sound->setIsLooped(loop); }
+
+			void Update(Vector3 position)
+			{
+				if (!sound)
+					return;
+				sound->setPosition(position);
+			}
+
+			void UpdateParams()
+			{
+				if (!sound)
+					return;
+				sound->setMinDistance(minDistance);
+				sound->setMaxDistance(maxDistance);
+				sound->setVolume(volume);
+				sound->setIsLooped(isLoop);
+			}
+
+			void Play()
+			{
+				if (!sound)
+					return;
+				sound->setPlayPosition(0);
+				if (sound->isFinished())
+				{
+					InitSound(soundFile,isLoop);
+					//AudioSystem::Play(sound->getSoundSource(), Vector3(sound->getPosition()), isLoop);
+				}
+				if (sound->getIsPaused())
+				{
+					sound->setIsPaused(false);
+				}
+			}
+
+			void Pause()
+			{
+				if (!sound)
+					return;
+				sound->setIsPaused(true);
+			}
+			void Unpause()
+			{
+				if (!sound)
+					return;
+				sound->setIsPaused(false);
+			}
+			bool GetIsPaused()
+			{
+				return sound->getIsPaused();
+			}
+
+			void Reset()
+			{
+				if (!sound)
+					return;
+				sound->setPlayPosition(0);
+			}
+
+			void Stop()
+			{
+				if (!sound)
+					return;
+				sound->stop();
+			}
+
+		private:
+			ISound* sound;
+			ISoundEffectControl* fxControl;
+			string soundFile;
+			float minDistance = 4.0f;
+			float maxDistance = 10.0f;
+			float volume = 1.0f;
+			bool isLoop = false;
+		};
+	}
+}
+
+
