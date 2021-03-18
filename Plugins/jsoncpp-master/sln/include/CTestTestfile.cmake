174be6eee3ab4f7f3b0cad3480ec5ff1a1991d98
Penguin <t.meng3@newcastle.ac.uk>
2021-03-19 07:35:48 +0800

    adding lost files

---
+++ Plugins/jsoncpp-master/sln/include/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: F:/Projects/CSC8508/jsoncpp-master/include
+# Build directory: F:/Projects/CSC8508/jsoncpp-master/build/include
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
