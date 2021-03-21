144ae5a7dab1991af36e84d71d7feaed820aafd8
Penguin <t.meng3@newcastle.ac.uk>
2021-03-22 05:13:37 +0800

    improve pushdown machine

--- CSC8503/CSC8503Common/PushdownMachine.cpp
+++ CSC8503/CSC8503Common/PushdownMachine.cpp
@@ -39,46 +39,67 @@
 
 
 # include "PushdownMachine.h" 
- # include "PushdownState.h" 
-using namespace NCL :: CSC8503 ;
- bool PushdownMachine::Update(float dt) {
-	 if (activeState) {
-		 PushdownState * newState = nullptr;
-		 PushdownState::PushdownResult result
-			 = activeState -> OnUpdate(dt, &newState);
-		
-			switch (result) {
-			 case PushdownState::Pop: {
-				 activeState -> OnSleep();
-				 delete activeState;
-				 stateStack.pop();
-				 if (stateStack.empty()) {
-					 return false;
-					
-				}
-				 else {
-					 activeState = stateStack.top();
-					 activeState -> OnAwake();
-					
-				}
-				 } break;
-			 case PushdownState::Push: {
-				  activeState -> OnSleep();
-				 
-					  stateStack.push(newState);
-				  activeState = newState;
-				  activeState -> OnAwake();
-				  } break;
-				 
+# include "PushdownState.h" 
+using namespace NCL::CSC8503;
+bool PushdownMachine::Update(float dt) {
+	if (activeState) {
+		PushdownState* newState = nullptr;
+		PushdownState::PushdownResult result
+			= activeState->OnUpdate(dt, &newState);
+
+		switch (result) {
+		case PushdownState::Pop: {
+			activeState->OnSleep();
+			delete activeState;
+			stateStack.pop();
+			if (stateStack.empty()) {
+				return false;
+
 			}
-			
-	 }
-	  else {
-		  stateStack.push(initialState);
-		  activeState = initialState;
-		  activeState -> OnAwake();
-		 
-	 }
-	  return true;
-	 
- }
+			else {
+				activeState = stateStack.top();
+				activeState->OnAwake();
+
+			}
+		} break;
+		case PushdownState::Push: {
+			activeState->OnSleep();
+
+			stateStack.push(newState);
+			activeState = newState;
+			activeState->OnAwake();
+		} break;
+
+		}
+
+	}
+	else {
+		stateStack.push(initialState);
+		activeState = initialState;
+		activeState->OnAwake();
+
+	}
+	return true;
+
+}
+
+bool NCL::CSC8503::PushdownMachine::Reset()
+{
+	if (initialState == nullptr)
+		return false;
+	while (activeState != initialState)
+	{
+		activeState->OnSleep();
+		stateStack.pop();
+		delete activeState;
+		if (stateStack.empty()) {
+			activeState = nullptr;
+			return false;
+		}
+		else {
+			activeState = stateStack.top();
+			activeState->OnAwake();
+		}
+	}
+	return true;
+}
--- CSC8503/CSC8503Common/PushdownMachine.h
+++ CSC8503/CSC8503Common/PushdownMachine.h
@@ -22,28 +22,28 @@
 //}
 
 # pragma once
- # include <stack>
- namespace NCL {
-	 namespace CSC8503 {
-		 class PushdownState;
-		 class PushdownMachine {
-		 public:
-			 PushdownMachine(PushdownState* initialState) {
-				 this->initialState = initialState;
-				
+# include <stack>
+namespace NCL {
+	namespace CSC8503 {
+		class PushdownState;
+		class PushdownMachine {
+		public:
+			PushdownMachine(PushdownState* initialState, PushdownState* activeState = nullptr) {
+				this->initialState = initialState;
+				this->activeState = activeState;
 			}
-			 ~PushdownMachine() {}
-			
-				 bool Update(float dt);
-			
-		 protected:
-			 PushdownState* activeState;
-			 PushdownState* initialState;
-			
+			~PushdownMachine() {}
+
+			bool Update(float dt);
+			bool Reset();
+		protected:
+			PushdownState* activeState;
+			PushdownState* initialState;
+
 			std::stack <PushdownState*> stateStack;
-			
+
 		};
-		
+
 	}
-	
+
 }
