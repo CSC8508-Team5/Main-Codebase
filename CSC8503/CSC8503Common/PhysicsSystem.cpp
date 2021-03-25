947ccf19d10d6dd3d075f099e3bbd3a33ff799ce
Penguin <t.meng3@newcastle.ac.uk>
2021-03-25 09:07:22 +0800

    fallback UpdateCollisionList behaviour

--- CSC8503/CSC8503Common/PhysicsSystem.cpp
+++ CSC8503/CSC8503Common/PhysicsSystem.cpp
@@ -288,23 +288,23 @@ rocket launcher, gaining a point when the player hits the gold coin, and so on).
 void PhysicsSystem::UpdateCollisionList() {
 	for (std::set<CollisionDetection::CollisionInfo>::iterator i = allCollisions.begin(); i != allCollisions.end(); ) {
 		if ((*i).framesLeft == numCollisionFrames) {
-			/*
+			
 			i->a->OnCollisionBegin(i->b);
 			i->b->OnCollisionBegin(i->a);
-			*/
+			
 			//change behaviour of collision trigger
-			i->a->AddCollisionObject(i->b);
-			i->b->AddCollisionObject(i->a);
+			//i->a->AddCollisionObject(i->b);
+			//i->b->AddCollisionObject(i->a);
 		}
 		(*i).framesLeft = (*i).framesLeft - 1;
 		if ((*i).framesLeft < 0) {
-			/*
+			
 			i->a->OnCollisionEnd(i->b);
 			i->b->OnCollisionEnd(i->a);
-			*/
+			
 			//change behaviour of collision trigger
-			i->a->RemoveCollisionObject(i->b);
-			i->b->RemoveCollisionObject(i->a);
+			//i->a->RemoveCollisionObject(i->b);
+			//i->b->RemoveCollisionObject(i->a);
 
 			i = allCollisions.erase(i);
 		}
