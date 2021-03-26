4affc9eb9be67a02d604131101803aea00c6e97f
Penguin <t.meng3@newcastle.ac.uk>
2021-03-26 18:59:26 +0800

    reduce air acceleration

--- CSC8503/GameTech/PlayerObject.h
+++ CSC8503/GameTech/PlayerObject.h
@@ -276,7 +276,7 @@ namespace NCL
 			Vector3 velocity = Vector3::Zero();
 			Vector3 desiredVelocity = Vector3::Zero();
 			float maxGroundAcceleration = 60.0f;
-			float maxAirAcceleration = 30.0f;
+			float maxAirAcceleration = 20.0f;
 			KeyMapping keys;
 		};
 	}
